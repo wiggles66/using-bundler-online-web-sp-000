06c26d3d0c4b64a2a4f59c6a50b01017ad237de6
Michael Waide <mvwaide@gmail.com>
2020-07-30 17:42:08 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,2 @@
-source 'https://rubygems.org'
-
-gem 'hashie'
-gem 'sinatra', '1.4.4'
-gem 'octokit', '~> 2.0'
-gem 'awesome_print', git: 'git://github.com/michaeldv/awesome_print.git'
-
-gem 'pry', group: :development
-
-group :test do
-  gem 'rspec'
-end
+require 'bundler/setup'
+Bundler.require(:default, :development)
